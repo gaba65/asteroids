8f9bbe8bd41149fc66eeff07f010e5f8549123d5
gaba65 <opsgabi65@gmail.com>
2024-08-23 13:40:01 +0300

    added the draw function to draw the player on-screen

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import pygame
-
+from player import Player
 from constants import *
 
 def main():
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
+   
     fps = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while 0 == 0:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
         dt = fps.tick(60) / 1000
 
 
